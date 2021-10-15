f91ec43e812cbbd87a2ffe0433ddfce1678fb3c9
Arthur <poubelle@arthur-maronian.fr>
2021-10-15 19:41:06 +0200

    creating users

--- internal/user/user.go
+++ internal/user/user.go
@@ -1,5 +1,11 @@
 package user
 
+import (
+	"database/sql"
+
+	"github.com/google/uuid"
+)
+
 type User struct {
 	Name string
 	Id   string
@@ -19,4 +25,24 @@ func getUserbyName() {
 
 func New(name string, id string) User {
 	return User{name, id}
+}
+
+func createUser(name string) {
+	id := uuid.New().String()
+
+	user := New(name, id)
+	db, err := sql.Open("mysql", "gochess:password@/dbname")
+	if err != nil {
+		panic(err)
+	}
+	db.r
+
+}
+
+func (user *User) setName(name string) {
+	user.Name = name
+}
+
+func (user *User) setID(id string) {
+	user.Id = id
 }
